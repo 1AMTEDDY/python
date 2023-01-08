1da655981f9df965915848db8ab7bef3112a0ddb
Peterson Nwoko <103382847+1AMTEDDY@users.noreply.github.com>
2023-01-09 00:15:15 +0100

    IPO and added a lil README

--- FirstPyCode.py
+++ FirstPyCode.py
@@ -22,6 +22,7 @@ print("Hi, I'm Peterson!," + " i am a Devops Engineer" + " learning Python")
 print("hi i'm" +  name + " I am a DevOps Engineer" + " 24 years old")
 
 #string interpolation add the f and parentheses to the code or use the .format method or you string coerce
+#this is the process of substituting the values of the variables into placeholder strings.
 print(f"Hi, name's {name} i'm {age} in {level} working as a {Job} Engineer and i'm a man that is {man}")
 #or
 print("Hi, name's {} i'm {} in {} working as a {} Engineer and i'm a man that is {}".format(name, age, level, Job, man))
---
+++ README.md
@@ -0,0 +1,4 @@
+# WHAT THIS REPO IS
+
+- A repository showing my Journey into learning about python.
+i'll update this as i go
