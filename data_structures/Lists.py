efb301581748abff2c4dc21bddb501e1f30eda64
Peterson Nwoko <103382847+1AMTEDDY@users.noreply.github.com>
2023-01-26 16:25:22 +0100

    DS

---
+++ data_structures/Lists.py
@@ -0,0 +1,13 @@
+#without structure, there's only chaos
+# A data structure is a particular way of organizing data in a computer so that it can be used effectively
+# A list is a a DS in python that is a mutable, or changeable, its an ORDERED sequence of elements. Each element or value in the list is called an item. lists are defined by having values in a square bracket. []
+our_list = [] #empty list
+full_list = ["Peter", 24, 3.14, False]
+print(full_list)
+#List Referencing, starting from 0 indexing
+my_name = full_list[0]
+print(my_name)
+
+pi = full_list[2]
+print(pi)
+
