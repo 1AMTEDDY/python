983c97c899347918d034ca5430a79bdd97cfabb0
Peterson Nwoko <103382847+1AMTEDDY@users.noreply.github.com>
2023-02-09 10:28:25 +0100

    Express yourself

---
+++ ops&exp/comparison.py
@@ -0,0 +1,22 @@
+#they return a boolean value
+# == equality
+# != Inequality
+# > Greater Than
+# < Less Than
+# >= Greater Than Equal to
+# <= Less Than/Equal
+
+print(5 == 5)
+print(5 != 5)
+print(5 != 6)
+print(6 > 5)
+print(5 == 5.0)
+print(5 == '5') #does some type checking
+print('Hello' == 'Hello')
+print('Hello' == 'hello') # would return false
+print(5 > len('five')) # you can also typecast
+print(5 < 6)
+print(5 >= 6) # would return false, if one condition returns true the whole thing is true
+print(5 >= 5) # returns true
+
+
